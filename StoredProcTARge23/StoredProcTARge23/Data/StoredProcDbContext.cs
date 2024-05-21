bd5f428c401c8f3884164c7087faf8ec8bfb7e48
riinojassoo <riin.ojassoo@hotmail.com>
2024-05-21 15:24:08 +0300

    init

---
+++ StoredProcTARge23/StoredProcTARge23/Data/StoredProcDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoredProcTARge23.Models;
+
+namespace StoredProcTARge23.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
