942ea740f6d641c1c743a850906e20160ff8fa49
riinojassoo <riin.ojassoo@hotmail.com>
2024-05-21 16:00:54 +0300

    searchResult

--- StoredProcTARge23/StoredProcTARge23/Controllers/EmployeeController.cs
+++ StoredProcTARge23/StoredProcTARge23/Controllers/EmployeeController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProcTARge23.Data;
+using StoredProcTARge23.Models;
 
 namespace StoredProcTARge23.Controllers
 {
     public class EmployeeController : Controller
     {
+        public StoredProcDbContext _context;
+        
+        public EmployeeController
+            (
+                StoredProcDbContext context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
         }
     }
 }
